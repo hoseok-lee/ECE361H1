5c50e6102a01bd1d3a1b410f07191c1702fbe182
Ho Seok Lee <hoseok.lee@mail.utoronto.ca>
2021-09-20 12:37:56 -0400

    Finished FileTransferLab section 1.

--- FileTransferLab/section-1/deliver.c
+++ FileTransferLab/section-1/deliver.c
@@ -15,28 +15,28 @@
 	
 int main(int argc, char **argv)
 {
-    int sockfd, port;
+    int sockfd;
+    struct addrinfo hints, *servinfo, *p;
 	struct sockaddr_in serv_addr;
+    socklen_t addr_len;
+	char buf[MAXBUFLEN] = { '\0' };
+	char filename[MAXBUFLEN] = { '\0' };
 
 
 
-    // deliver <server address> <server port number>
+    // Check for number of arguments
     if (argc != 3)
     {
+        perror("usage: deliver <server address> <server port number>");
         exit(0);
     }
 	
-	port = atoi(argv[2]);
-	// Server address information
-    memset(&hints, 0, sizeof hints);    // Reset struct to all zeros
-    hints.ai_family = AF_INET;          // Use IPv4
-    hints.ai_socktype = SOCK_DGRAM;     // UDP stream sockets
-    hints.ai_protocol = IPPROTO_UDP;    // Internet protocol suite for UDP
-    hints.ai_flags = AI_PASSIVE;        // Address of local host to sockets
+	memset(buf, 0, MAXBUFLEN);
+	memset(filename, 0, MAXBUFLEN);
 
     // Generate socket descriptor for binding
     // sockfd is the socket file descriptor
-    if ((sockfd = socket(hints.ai_family, hints.ai_socktype, 0)) == -1)
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
     {
         perror("talker: socket");
         exit(1);
@@ -44,56 +44,65 @@ int main(int argc, char **argv)
 
     // Server socket address information
 	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(port);
-	memset((char *) &serv_addr, 0, sizeof serv_addr);
+	serv_addr.sin_port = htons(atoi(argv[2]));
 	if (inet_aton(argv[1], &serv_addr.sin_addr) == 0)
     {
 		perror("talker: invalid address");
         exit(1);
 	}
 	
-	const int BUF_SIZE = 100;
-	char buf[BUF_SIZE] = {0};
-	char filename[BUF_SIZE] = {0};
-	
-    printf("Enter a filename in the format: ftp <file name>");
-	fgets(buf, BUF_SIZE, stdin);
-	
-	int i = 0;
-	while(buf[i] == ' '){
-		i++;
-	}
-	if(buf[i] == 'f' && buf[i + 1] == 't' && buf[i + 2] == 'p'){
-		i += 3;
-	while (buf[i] == ' '){ 
-		i++;
-	}
-	char *token = strtok(buf + cursor, "\r\t\n ");
-	strncpy(filename, token, BUF_SIZE);
-	} 
-	
-	if(access(filename, F_OK) == -1){
+    // Parse filename
+    printf("usage: ftp <file name> ");
+	fgets(buf, MAXBUFLEN, stdin);
+    
+    // Check if the first command is "ftp"
+    char *ptr = strtok(buf, " \n");
+    if (strcmp(ptr, "ftp") != 0)
+    {
+        printf("Invalid command.\n");
+        exit(1);
+    }
+
+    // Check if the next argument exists
+    if (ptr == NULL)
+    {
+        printf("Filename not provided.\n");
+        exit(1);
+    }
+
+    ptr = strtok(NULL, " \n");
+    strncpy(filename, ptr, MAXBUFLEN);
+
+    // Check if file with filename exists
+	if(access(filename, F_OK) == -1)
+    {
 		printf("File \"%s\" doesn't exist.\n", filename);
+        exit(1);
 	}
 	
-	int bytes = sendto(sockfd, "ftp", strlen("ftp"), 0, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
-	if (bytes == -1){
-		printf("Sending error\n");
+    // Send message to server
+    char *message = "ftp";
+	addr_len = sizeof serv_addr;
+	if ((sendto(sockfd, message, strlen(message), 0, (struct sockaddr *)&serv_addr, addr_len)) == -1)
+    {
+		perror("talker: sendto");
+        exit(1);
 	}
 	
-	memset(buf, 0, BUF_SIZE); 
-	socklen_t serv_addr_size = sizeof(serv_addr);
-    bytes = recvfrom(sockfd, buf, BUF_SIZE, 0, (struct sockaddr *) &serv_addr, &serv_addr_size))
-	if(bytes == -1){
-		printf("Receiving error\n");
+    // Receive message from client
+	if((recvfrom(sockfd, buf, MAXBUFLEN, 0, (struct sockaddr *) &serv_addr, &addr_len)) == -1)
+    {
+		perror("talker: recvfrom");
+        exit(1);
 	}
 	
-	if(strcmp(buf, "yes") == 0){
+    // File transfer can start once acknowledgement message has been sent
+	if (strcmp(buf, "yes") == 0)
+    {
 		printf("A file transfer can start\n");
 	}
 	
 	close(sockfd);
 	
 	return 0;
-	}
-    
+}
--- FileTransferLab/section-1/server.c
+++ FileTransferLab/section-1/server.c
@@ -15,102 +15,47 @@
 
 int main (int argc, char **argv)
 {
-    int sockfd, port;
+    int sockfd, numbytes;
     struct addrinfo hints, *servinfo, *p;
-    int yes = 1;
-    int rv, numbytes;
     struct sockaddr_in serv_addr, their_addr;
     socklen_t addr_len;
-    char buf[MAXBUFLEN];
+    char buf[MAXBUFLEN] = { '\0' };
 
 
 
-    // server <UDP listen port>
-    if (argc != 2) {
+    // Check for number of arguments
+    if (argc != 2) 
+    {
+        perror("usage: server <UDP listen port>");
         exit(0);
     }
 
-    port = atoi(argv[1]);
+	memset(buf, 0, MAXBUFLEN);
 
     // Server socket address information
     serv_addr.sin_family = AF_INET;                 // Use IPv4
-    serv_addr.sin_port = htons(port);               // Convert port number to hosthort 
+    serv_addr.sin_port = htons(atoi(argv[1]));               // Convert port number to hosthort 
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);  // Convert address request to hostlong
-
-    // Server address information
-    memset(&hints, 0, sizeof hints);    // Reset struct to all zeros
-    hints.ai_family = AF_INET;          // Use IPv4
-    hints.ai_socktype = SOCK_DGRAM;     // UDP stream sockets
-    hints.ai_protocol = IPPROTO_UDP;    // Internet protocol suite for UDP
-    hints.ai_flags = AI_PASSIVE;        // Address of local host to sockets
 
     // Generate socket descriptor for binding
     // sockfd is the socket file descriptor
-    if ((sockfd = socket(hints.ai_family, hints.ai_socktype, hints.ai_protocol)) == -1)
+    printf("creating socket...\n");
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
     {
         perror("listener: socket");
         exit(1);
     }
 
     // Bind socket to a port
+    printf("binding...\n");
     if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof serv_addr) == -1)
     {
-        close(sockfd);
         perror("listener: bind");
         exit(1);
     }
 
-    /*
-    // Server information
-    memset(&hints, 0, sizeof hints);    // Reset struct to all zeros
-    hints.ai_family = AF_UNSPEC;        // Can either use IPv4 or IPv6
-    hints.ai_socktype = SOCK_DGRAM;     // UDP stream sockets
-    hints.ai_flags = AI_PASSIVE;        // Assign the address of local host to socket structures
-
-    // Retrieve address information
-    // servinfo is a linked list of 1 or more struct addrinfos
-    if ((rv = getaddrinfo(NULL, argv[1], &hints, &servinfo)) != 0)
-    {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-        return 1;
-    }
-
-    // Loop through all the servers and bind to the first available one
-    for (p = servinfo; p!= NULL; p = p->ai_next)
-    {
-        // Generate socket descriptor for binding
-        // sockfd is the socket file descriptor
-        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) ==  -1)
-        {
-            perror("listeneder: socket");
-            continue;
-        }
-
-        // Bind to a port
-        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1)
-        {
-            close(sockfd);
-            perror("listener: bind");
-            continue;
-        }
-
-        // Allow binding of the first available port
-        // If all system calls and procedures have succeeded, stop searching
-        break;
-    }
-
-    // If the linked list was exhausted, then the server failed to bind to a socket
-    if (p == NULL)
-    {
-        fprintf(stderr, "listener: failed to bind\n");
-        exit(1);
-    }
-
-    // Free the linked list of struct addrinfos
-    freeaddrinfo(servinfo);
-    */
-
     // Listening...
+    printf("listening...\n");
     addr_len = sizeof their_addr;
     if ((numbytes = recvfrom(sockfd, buf, MAXBUFLEN - 1, 0, (struct sockaddr *)&their_addr, &addr_len)) == -1)
     {
