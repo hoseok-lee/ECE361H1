64afe00fa7d9ec40f21231601ba79165a3da7e70
hoseok-lee <47509132+hoseok-lee@users.noreply.github.com>
2021-11-26 19:00:39 -0500

    Implemented linked list with user information.

--- TextConferencingLab/section-2/server/server.c
+++ TextConferencingLab/section-2/server/server.c
@@ -20,7 +20,20 @@
 // Message
 #include "../message.h"
 
-#define MAX_USERS 4
+
+
+// Linked lsit
+typedef struct user_info
+{
+    char username[MAX_NAME];
+    char password[MAX_NAME];
+
+    Users user;
+
+    struct user_info * next_user;
+} UserInfo;
+
+
 
 int main (int argc, char ** argv)
 {
@@ -29,7 +42,55 @@ int main (int argc, char ** argv)
     struct sockaddr_storage addr;
     socklen_t addr_len;
     char package[MESSAGE_MAXBUFLEN];
+
+    FILE * user_list_fp;
+    if ((user_list_fp = fopen("user_list.txt", "r")) == NULL) 
+    {
+        perror("init: fopen");
+    }
+
+    // Retrieve dynamic linked list user list
+    UserInfo * user_list_head = NULL, * prev = NULL, * curr = NULL;
+    while (true)
+    {
+        // Set the current node to the previous node for the next iteration
+        prev = curr;
+        // Create new set of user information
+        UserInfo * new_user = malloc(sizeof(UserInfo));
+        if (fscanf(user_list_fp, "%s %s\n", new_user->username, new_user->password) == EOF)
+        {
+            free(new_user);
+            break;
+        }
+
+        // Set current node
+        curr = new_user;
+        // Link previous to current
+        if (prev != NULL)
+        {
+            prev->next_user = curr;
+        }
+        // Create head in the case for the first initialization
+        // prev will always be NULL on the first iteration, and filled afterwards
+        else 
+        {
+            user_list_head = curr;
+        }
+    }
+
+    curr = user_list_head;
+    while (curr != NULL)
+    {
+        printf("%s", curr->username);
+        curr = curr->next_user;
+    }
+
+
+    const int MAX_USERS = 4;
+
     Users user_list[MAX_USERS];
+
+
 
     // Userenames and passwords
     char * username_database[MAX_USERS] = {
@@ -117,6 +178,18 @@ int main (int argc, char ** argv)
         user_list[i].port_number = -1;
     }
 
+    curr = user_list_head;
+    while (curr != NULL)
+    {
+        curr->user.socket = -1;
+        strcpy(curr->user.ip_address, "\0");
+        strcpy(curr->user.name, "\0");
+        strcpy(curr->user.session_ID, "\0");
+        curr->user.port_number = -1;
+
+        curr = curr->next_user;
+    }
+
     while (true)
     {
         // Update the current list of sockets from master
@@ -182,13 +255,16 @@ int main (int argc, char ** argv)
 
                 // Check whether the user has already logged in or not
                 bool logged = false;
-                for (int j = 0; j < MAX_USERS; ++j)
+                curr = user_list_head;
+                while (curr != NULL)
                 {
-                    if (user_list[j].socket == i)
+                    if (curr->user.socket == i)
                     {
                         logged = true;
                         break;
                     }
+
+                    curr = curr->next_user;
                 }
 
                 // User attempts to log in and is currently not already logged in
@@ -201,47 +277,42 @@ int main (int argc, char ** argv)
 
                     // Check if user is already in the list of users
                     bool online = false;
-                    for (int j = 0; j < MAX_USERS; ++j)
+                    curr = user_list_head;
+                    while (curr != NULL)
                     {
-                        if (strcmp(user_list[j].name, username) == 0)
+                        if (strcmp(curr->user.name, username) == 0)
                         {
                             online = true;
                             break;
                         }
+
+                        curr = curr->next_user;
                     }
 
                     // Check for credentials in user database
                     bool user_found = false;
-                    for (int j = 0; (j < MAX_USERS) && (online == false); ++j)
+                    curr = user_list_head;
+                    while ((curr != NULL) && (online == false))
                     {
                         // Match found
-                        if ((strcmp(username_database[j], username) == 0) && \
-                            (strcmp(password_database[j], password) == 0))
+                        if ((strcmp(curr->username, username) == 0) && \
+                            (strcmp(curr->password, password) == 0))
                         {
-                            // Find the first available user spot
-                            for (int k = 0; k < MAX_USERS; ++k)
-                            {
-                                // Socket is not yet set
-                                if (user_list[k].socket == -1)
-                                {
-                                    // Get their socket information
-                                    struct sockaddr_in their_addr;
-                                    socklen_t addr_len = sizeof their_addr;
-                                    getpeername(i, (struct sockaddr *)&their_addr, &addr_len);
+                            // Get their socket information
+                            struct sockaddr_in their_addr;
+                            socklen_t addr_len = sizeof their_addr;
+                            getpeername(i, (struct sockaddr *)&their_addr, &addr_len);
 
-                                    char ip_address[INET6_ADDRSTRLEN];
-                                    inet_ntop(AF_INET, &(their_addr.sin_addr), ip_address, INET_ADDRSTRLEN);
+                            char ip_address[INET6_ADDRSTRLEN];
+                            inet_ntop(AF_INET, &(their_addr.sin_addr), ip_address, INET_ADDRSTRLEN);
 
-                                    // Update user information
-                                    user_list[k].socket = i;
-                                    strcpy(user_list[k].ip_address, ip_address);
-                                    strcpy(user_list[k].name, username);
-                                    strcpy(user_list[i].session_ID, "\0");
-                                    user_list[k].port_number = their_addr.sin_port;
-                                    break;
-                                }
-                            }
-
+                            // Update user information
+                            curr->user.socket = i;
+                            strcpy(curr->user.ip_address, ip_address);
+                            strcpy(curr->user.name, username);
+                            strcpy(curr->user.session_ID, "\0");
+                            curr->user.port_number = their_addr.sin_port;
+                            
                             // Send acknowledgement message
                             serv_message.type = LO_ACK;
                             serv_message.size = 0;
@@ -250,6 +321,8 @@ int main (int argc, char ** argv)
                             user_found = true;
                             break;
                         }
+
+                        curr = curr->next_user;
                     }
 
                     // If no user was found, send a NACK packet
@@ -277,13 +350,16 @@ int main (int argc, char ** argv)
 
                     // Chcek if session exists
                     bool session_exists = false;
-                    for (int j = 0; j < MAX_USERS; ++j)
+                    curr = user_list_head;
+                    while (curr != NULL)
                     {
-                        if (strcmp(user_list[j].session_ID, session_ID) == 0)
+                        if (strcmp(curr->user.session_ID, session_ID) == 0)
                         {
                             session_exists = true;
                             break;
                         }
+
+                        curr = curr->next_user;
                     }
 
                     if (session_exists == true) 
@@ -292,14 +368,17 @@ int main (int argc, char ** argv)
                         serv_message.type = JN_ACK;
                         serv_message.size = 0;
 
-                         // Add user to the session
-                        for (int j = 0; j < MAX_USERS; ++j)
+                        // Add user to the session
+                        curr = user_list_head;
+                        while (curr != NULL)
                         {
-                            if (user_list[j].socket == i)
+                            if (curr->user.socket == i)
                             {
-                                strcpy(user_list[j].session_ID, session_ID);
+                                strcpy(curr->user.session_ID, session_ID);
                                 break;
                             }
+
+                            curr = curr->next_user;
                         }
                     }
                     // Send a NACK if session does not exist
@@ -325,14 +404,17 @@ int main (int argc, char ** argv)
                 else if (type == LEAVE_SESS)
                 {
                     // Remove user from the session
-                    for (int j = 0; j < MAX_USERS; ++j)
+                    curr = user_list_head;
+                    while (curr != NULL)
                     {
                         // Reset to default
-                        if (user_list[j].socket == i)
+                        if (curr->user.socket == i)
                         {
-                            strcpy(user_list[j].session_ID, "\0");
+                            strcpy(curr->user.session_ID, "\0");
                             break;
                         }
+                        
+                        curr = curr->next_user;
                     }
                 }
                 else if (type == NEW_SESS)
@@ -341,13 +423,16 @@ int main (int argc, char ** argv)
                     char * session_ID = strtok(NULL, "\n");
 
                     // Update user
-                    for (int j = 0; j < MAX_USERS; ++j)
+                    curr = user_list_head;
+                    while (curr != NULL)
                     {
-                        if (user_list[j].socket == i)
+                        if (curr->user.socket == i)
                         {
-                            strcpy(user_list[j].session_ID, session_ID);
+                            strcpy(curr->user.session_ID, session_ID);
                             break;
                         }
+
+                        curr = curr->next_user;
                     }
 
                     // Send acknowledgement
@@ -367,23 +452,26 @@ int main (int argc, char ** argv)
                 {
                     memset(serv_message.data, 0, MAX_DATA);
                     // Generate a list of all users
-                    for (int j = 0; j < MAX_USERS; ++j)
+                    curr = user_list_head;
+                    while (curr != NULL)
                     {
                         // As long as the user entry is not empty
-                        if (strcmp(user_list[j].name, "\0") != 0)
+                        if (strcmp(curr->user.name, "\0") != 0)
                         {
-                            strcat(serv_message.data, user_list[j].name);
+                            strcat(serv_message.data, curr->user.name);
                             strcat(serv_message.data, " <");
-                            if (strcmp(user_list[j].session_ID, "\0") == 0)
+                            if (strcmp(curr->user.session_ID, "\0") == 0)
                             {
                                 strcat(serv_message.data, "no session");
                             }
                             else
                             {
-                                strcat(serv_message.data, user_list[j].session_ID);
+                                strcat(serv_message.data, curr->user.session_ID);
                             }
                             strcat(serv_message.data, ">\n");
                         }
+
+                        curr = curr->next_user;
                     }
 
                     printf("%s", serv_message.data);
@@ -408,17 +496,20 @@ int main (int argc, char ** argv)
                     FD_CLR(i, &master);
 
                     // Remove client from user list
-                    for (int j = 0; j < MAX_USERS; ++j) 
+                    curr = user_list_head;
+                    while (curr != NULL) 
                     {
                         // Reset user list entry
-                        if (user_list[j].socket == i) {
-                            user_list[j].socket = -1;
-                            strcpy(user_list[j].ip_address, "\0");
-                            strcpy(user_list[j].name, "\0");
-                            strcpy(user_list[i].session_ID, "\0");
-                            user_list[j].port_number = -1;
+                        if (curr->user.socket == i) {
+                            curr->user.socket = -1;
+                            strcpy(curr->user.ip_address, "\0");
+                            strcpy(curr->user.name, "\0");
+                            strcpy(curr->user.session_ID, "\0");
+                            curr->user.port_number = -1;
                             break;
                         }
+
+                        curr = curr->next_user;
                     }
                 }
                 else if (type == MESSAGE)
@@ -434,36 +525,40 @@ int main (int argc, char ** argv)
                     serv_message.size = strlen(serv_message.data);
 
                     // Check which session the client is a part of
-                    int session_sock;
-                    for (int j = 0; j < MAX_USERS; ++j)
+                    UserInfo * sender;
+                    curr = user_list_head;
+                    while (curr != NULL)
                     {
-                        if (user_list[j].socket == i)
+                        if (curr->user.socket == i)
                         {
-                            session_sock = j;
+                            sender = curr;
+                            break;
                         }
+
+                        curr = curr->next_user;
                     }
 
-                    // Skip clients that are not part of any session
-                    if (strcmp(user_list[session_sock].session_ID, "\0") == 0)
-                    {
-                        continue;
-                    }
+                    printf("reached");
 
                     // Iterate through all sockets and determine which one shares sessions
-                    for (int j = 0; j < MAX_USERS; ++j)
+                    curr = user_list_head;
+                    while (curr != NULL)
                     {
                         // But don't send to itself
-                        if ((strcmp(user_list[j].session_ID, user_list[session_sock].session_ID) == 0) && (j != session_sock))
+                        if ((strcmp(curr->user.session_ID, sender->user.session_ID) == 0) && \
+                            (curr != sender))
                         {
                             // Pack and send message
                             char * packed_message = pack_message(&serv_message);
-                            if (send(user_list[j].socket, packed_message, strlen(packed_message), 0) == -1)
+                            if (send(curr->user.socket, packed_message, strlen(packed_message), 0) == -1)
                             {
                                 perror("client (MESSAGE): send");
                                 exit(1);
                             }
                             free(packed_message);
                         }
+
+                        curr = curr->next_user;
                     }
                 }
             }
@@ -472,6 +567,7 @@ int main (int argc, char ** argv)
 
     freeaddrinfo(res);
     close(sockfd);
+    fclose(user_list_fp);
 
     return 0;
 }
